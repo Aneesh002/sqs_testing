54e36bfd73861bf116752b06c0071349c177fa28
Anish <anishshahkumar@gmail.com>
2024-02-07 21:48:25 +0545

    update

--- sqs_queue/client.py
+++ sqs_queue/client.py
@@ -6,30 +6,27 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
-def get_sqs_client(region_name, aws_sqs_key_id, aws_sqs_secret_access_key):
+
+def get_sqs_client(region_name, aws_access_key_id, aws_secret_access_key):
     """
     Creates and returns an AWS SQS client object using the specified credentials and region. This client
     is used for interacting with AWS SQS, including operations like sending and receiving messages from a
     specified AWS SQS QUEUE.
 
-    :param region_name:(str) The name of the AWS region where the SQS service is located
-    :param aws_sqs_key_id:(str) The AWS access key ID, part of the credentials to authenticate with AWS
-    :param aws_sqs_secret_access_key:(str) The AWS secret access key, part of the credentials to authenticate
-                                    with AWS.
+    :param region_name: (str) The name of the AWS region where the SQS service is located.
+    :param aws_access_key_id: (str) The AWS access key ID, part of the credentials to authenticate with AWS.
+    :param aws_secret_access_key: (str) The AWS secret access key, part of the credentials to authenticate with AWS.
     :return: A boto3 SQS client object configured with the provided credentials and region,
-            ready for interacting with AWS SQS and Returns None if the client creation fails due to
-            missing credentials or other
+             ready for interacting with AWS SQS. Returns None if the client creation fails due to
+             missing credentials or other errors.
     """
-
-    if not (aws_sqs_key_id and aws_sqs_secret_access_key):
-        raise ValueError("AWS access key and secret key must be provided")
 
     try:
         sqs_client = boto3.client(
             'sqs',
             region_name=region_name,
-            aws_sqs_key_id=aws_sqs_key_id,
-            aws_sqs_secret_access_key=aws_sqs_secret_access_key
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key  # Corrected parameter name
         )
         logger.info("Successfully created SQS client.")
         return sqs_client
@@ -38,4 +35,3 @@ def get_sqs_client(region_name, aws_sqs_key_id, aws_sqs_secret_access_key):
         return None
     except ClientError as e:
         logger.error("An error occurred while creating AWS SQS client: %s", e)
-        return None
