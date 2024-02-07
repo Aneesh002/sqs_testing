1ef94ab25e2dbbbf852e6020af8332097a0459c3
Anish <anishshahkumar@gmail.com>
2024-02-07 21:49:38 +0545

    update

--- sqs_queue/message_operations.py
+++ sqs_queue/message_operations.py
@@ -1,34 +1,39 @@
 import json
 import logging
+import ast
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
-def send_messages_batch(sqs_client, queue_url, batch):
+def send_messages_batch(sqs_client, sqs_queue_url, batch):
     """
     This function is responsible to send the message in the batch of 10 messages as an entries to the sqs queue.
 
     :param sqs_client: (obj) Initialized client object for accessing the AWS SQS queue.
-    :param queue_url: The URL of the AWS SQS queue from which to fetch messages.
+    :param QueueUrl: The URL of the AWS SQS queue from which to fetch messages.
     :param batch: This is the batch of 10 messages that is for sending to the AWS SQS QUEUE
     :return: None ,This function does not return value but logs the information of processing i.e. success or errors.
     """
     try:
-        response = sqs_client.send_message_batch(QueueUrl=queue_url, Entries=batch)  # We must pass the batch to Entries
-        for success in response.get('Successful', []):
-            # MangoDB update
-            logger.info(f"Message sent successfully: {success['MessageId']}")
-        for failure in response.get('Failed', []):
-            logger.error(f"Failed to send message: {failure['Id']}")
-            # MangoDB update
+        response = sqs_client.send_message_batch(QueueUrl=sqs_queue_url, Entries=batch)
 
-    # except ClientError as e:
+        # Log information about successfully sent messages
+        successful = response.get('Successful', [])
+        for success in successful:
+            logger.info(f"Message with ID {success['Id']} sent successfully. AWS MessageId: {success['MessageId']}")
+
+        # Log information about failed messages
+        failed = response.get('Failed', [])
+        for failure in failed:
+            logger.error(
+                f"Failed to send message with ID {failure['Id']}. Error code: {failure['Code']}. Error message: {failure['Message']}")
+
     except Exception as e:
-        logger.error(f"An error occurred: {e}")
+        logger.error("An error occurred: %s", e)
 
 
-def receive_sqs_message(sqs_client, queue_url, max_num):
+def receive_sqs_message(sqs_client, QueueUrl, max_num):
     """
     This function is responsible for receiving  as specified no of the maximum message i.e 1 as defined ,
     from the AWS SQS QUEUE ,It waits for the messages up to 20 sec after if it does not receive within that time
@@ -36,7 +41,7 @@ def receive_sqs_message(sqs_client, queue_url, max_num):
     deserializes it from a JSON string to a Python dictionary, and logs the message ID
 
     :param sqs_client: (object) The SQS Client object configured for accessing the AWS SQS QUEUE
-    :param queue_url:(str) The URL of the AWS SQS queue from which messages are to be received.
+    :param QueueUrl:(str) The URL of the AWS SQS queue from which messages are to be received.
     :param max_num: The maximum number of messages to receive from the queue in one call.
     :return: tuple,A tuple containing the receipt handle, message ID, and the message body as a dictionary if a message is
             successfully received. If the queue is empty, returns a message indicating the empty queue, `None` for
@@ -44,27 +49,28 @@ def receive_sqs_message(sqs_client, queue_url, max_num):
     """
 
     try:
-        response = sqs_client.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=max_num, WaitTimeSeconds=20)
+        response = sqs_client.receive_message(QueueUrl=QueueUrl, MaxNumberOfMessages=max_num, WaitTimeSeconds=20)
         message = response.get('Messages', [])
         if message:
             # Deserialize the message body from JSON string to Python dictionary
             message = message[0]
             message_body: dict = json.loads(message['Body'])
-            logger.info(f"Message id {message['id']} fetched successfully.")
-            return message['ReceiptHandle'], message['id'], message_body  # message['ReceiptHandle'] is string type.
+            message['Body'] = ast.literal_eval(message["Body"])
+            logger.info(f"Message guid {message['Body']['guid']} fetched successfully.")
+            return message['ReceiptHandle'], message['Body']['guid'], message_body  # message['ReceiptHandle'] is string type.
         return "The AWS SQS Queue is empty", None, None
     except Exception as e:
         logger.info(f"Error receiving message: {e}")
         return None, None, None
 
 
-def sqs_delete_message(sqs_client, queue_url, receipt_handle):
+def sqs_delete_message(sqs_client, QueueUrl, receipt_handle):
     """
     This function is responsible for deleting the message from the AWS SQS QUEUE Using the message's receipt handle .
 
     :param sqs_client:(str)The SQS client object configured for accessing Amazon SQS which have permissions to delete
                      messages from the specified SQS queue.
-    :param queue_url:(str) The URL of the SQS queue from which the message is to be deleted.
+    :param QueueUrl:(str) The URL of the SQS queue from which the message is to be deleted.
     :param receipt_handle: The receipt handle of the message to be deleted which is a unique identifier for the
                     receipt of the message by the client from the queue and is different from the message ID.
 
@@ -72,9 +78,8 @@ def sqs_delete_message(sqs_client, queue_url, receipt_handle):
             the actual implementation
     """
     try:
-        response = sqs_client.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
+        response = sqs_client.delete_message(QueueUrl=QueueUrl, ReceiptHandle=receipt_handle)
         # logging.info(f'Deleted the message {message_id} from the AWS SQS Queue.')
         return response
     except Exception as e:
         logger.info(f"Error while deleting message: {e}")
-
