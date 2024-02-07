b488b5c56edfe4dbd9e022287045df18870648cc
Anish <anishshahkumar@gmail.com>
2024-02-07 21:49:15 +0545

    create function of count

--- sqs_queue/count_message_in_queue.py
+++ sqs_queue/count_message_in_queue.py
@@ -1,16 +1,16 @@
-def count_messages_in_queue(sqs_client, sqs_queue_url):
+def count_messages_in_queue(sqs_client, QueueUrl) -> int:
     """
     This function is responsible for counting the number of message that are
     available currently in the AWS sqs queue for implementing the logic to have
     a look whether SQS queue is empty or not
 
     :param sqs_client:(object) The SQS Client object configured for accessing the AWS SQS QUEUE
-    :param sqs_queue_url:(str) The URL of the AWS SQS queue from which attributes messages is  to be received
+    :param QueueUrl:(str) The URL of the AWS SQS queue from which attributes messages is  to be received
     :return:(int)This function returns the integer value which is the no of approximate message in the AWS sqs queue
 
     """
     response = sqs_client.get_queue_attributes(
-        QueueUrl=sqs_queue_url,
+        QueueUrl=QueueUrl,
         AttributeNames=['ApproximateNumberOfMessages']
     )
     message_in_queue = int(response['Attributes']['ApproximateNumberOfMessages'])
